101a98d9b2df82f204b710c64ee0f0b0538f5f6c
David <daso.timileyin@gmail.com>
2023-06-25 14:50:52 +0100

    up de

--- api/account.go
+++ api/account.go
@@ -96,3 +96,50 @@ func (server *Server) listAccount(ctx *gin.Context){
 
 	ctx.JSON(http.StatusOK, accounts)
 }
+
+
+type updateAccountRequest struct {
+	ID int64 `json:"id" binding:"required,min=1"`
+	Balance int64 `json:"balance" binding:"required"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context){
+	var req updateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.UpdateAccountParams{
+		ID: req.ID,
+		Balance: req.Balance,
+	}
+
+	account, err := server.bank.UpdateAccount(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context){
+	var req deleteAccountRequest
+	if err := ctx.BindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.bank.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, "account deleted")
+
+}
